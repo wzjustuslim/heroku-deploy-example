4aa42014b44e41140f8227117bda4748e6bc6a5a
awongh <akira@awongh.com>
2020-12-10 12:41:28 +0800

    base express heroku node stuff

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+import express from 'express';
+
+const PORT = process.argv[2];
+
+// Initialise Express
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/bananas', (request, response) => {
+
+  const responseText = `This is a random number: ${Math.random()}`;
+
+  console.log('request came in', responseText);
+
+  const data = { responseText };
+
+  response.render('bananas', data);
+});
+
+app.listen(PORT);
